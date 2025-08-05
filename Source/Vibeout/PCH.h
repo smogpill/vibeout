1a89e4726fff525b5f9f9a5ab6b3ad62a28b99a8
Jounayd Id Salah <smogpill@gmail.com>
2025-08-05 22:57:03 +0200

    Fixed Vulkan crash because of bad version

--- Source/Vibeout/PCH.h
+++ Source/Vibeout/PCH.h
@@ -17,7 +17,7 @@
 #include <SDL3/SDL_vulkan.h>
 #include <vulkan/vulkan.h>
 #include <vulkan/vk_enum_string_helper.h>
-#define VMA_VULKAN_VERSION 1002000
+//#define VMA_VULKAN_VERSION 1003000
 #include <vk_mem_alloc.h>
 #define VC_EXTRALEAN
 #define NOLANGUAGE
